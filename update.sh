33baa1ebdf46526e50d42846a68d5cd0e88f3548
Mathias Kahr <m.kahr@student.tugraz.at>
2020-09-27 20:32:54 +0200

    testing upload command

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@ REMOTES=`git remote -v | grep "fetch" | wc -l`
 if [ $REMOTES -eq 1 ]
 then
     git remote add upstream git@gitlab.tugraz.at:esp-ws20/hello_world.git
+    git config pull.rebase false
 fi
 
 UNSTAGED=`git status --porcelain | wc -l`
